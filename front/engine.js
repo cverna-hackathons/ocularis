6de1fb7f6f5c6ed3be754ae8d00ff58c6c8f9587
Peter Berezny <spontain@gmail.com>
2016-01-20 21:52:01 +0100

    Added reset view to engine, closes #8

--- front/engine.js
+++ front/engine.js
@@ -8,25 +8,29 @@ module.exports = (() => {
       let box   = require('./models/box');
       let bb    = box();
 
-      ENGINE.camera   = require('./camera')(ENGINE);
+      ENGINE.resetView();
       ENGINE.scene    = require('./scene')(ENGINE);
       ENGINE.renderer = require('./renderer')(ENGINE);
       ENGINE.motion   = require('./motion')(ENGINE);
       ENGINE.draw     = require('./draw')(ENGINE);
       ENGINE.floor    = require('./floor')(ENGINE);
-
       ENGINE.scene.add(bb);
       ENGINE.scene.add(light);
       ENGINE.scene.add(ENGINE.floor);
-      ENGINE.frameUpdate = true;
+      
 
       $('#scene').html(ENGINE.renderer.domElement);
       ENGINE.draw();
 
-      $(document).ready(setTriggers)
+      $(document).ready(setTriggers);
+    },
+    resetView: () => {
+      ENGINE.camera = require('./camera')(ENGINE);
+      ENGINE.frameUpdate = true;
     }
   };
   let getEventKeyDirection = (event) => {
+    console.log('getEventKeyDirection | event.keyCode:', event.keyCode)
     let direction;
     switch (event.keyCode) {
       case 38:
@@ -41,19 +45,24 @@ module.exports = (() => {
       case 39:
         direction = 'right';
         break;
+      case 32:
+        direction = 'reset';
+        break;
     }
     return direction;
-  }
+  };
   let trackKeys = event => {
     let direction = getEventKeyDirection(event);
 
-    if (direction) ENGINE.motion.incite(direction);
+    if (direction && direction !== 'reset') 
+      ENGINE.motion.incite(direction);
   };
 
   let untrackKeys = event => {
     let direction = getEventKeyDirection(event)
 
-    if (direction) ENGINE.motion.impede(direction)
+    if (direction) 
+      ENGINE.motion.impede(direction)
   };
 
   let setTriggers = () => {
