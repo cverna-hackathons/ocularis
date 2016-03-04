6066bf91de36cb65931ea943e2363bbd9a4ecd70
Peter Berezny <spontain@gmail.com>
2016-03-04 17:18:42 +0100

    Removal from repo / tmp files

--- tmp-ui.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(init);
-
-function init() {
-
-  // This will render each component in preview thumbnail
-  $('.component-preview').each((elemIndex, previewElement) => {
-
-  });
-}
