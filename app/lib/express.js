65533aad8ed7fb614318b2789f844f21ea54fdaf
KandoSVK <kubo.kanitra@gmail.com>
2016-03-10 00:19:17 +0100

    Basic auth with passport + seed database script

--- app/lib/express.js
+++ app/lib/express.js
@@ -39,10 +39,11 @@ module.exports = function(app, config) {
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
-  var controllers = glob.sync(config.root + '/app/controllers/*.js');
-  controllers.forEach(function (controller) {
-    require(controller)(app);
-  });
+  //setup authentification
+  require('./auth/init')(app);
+
+  //setup routes
+  require('../controllers/routes')(app);
 
   app.use(function (req, res, next) {
     var err = new Error('Not Found');
